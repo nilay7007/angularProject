eaeb535aeb9ef300c365ba877f869422e6261b72
Nilay7007 <nilaysachan16@gmail.com>
2020-12-26 19:08:52 +0530

    Implemented routing for recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ResourceLoader } from '@angular/compiler';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,12 +11,23 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+ id:number;
+  constructor(private recipeService: RecipeService,
+    private route:ActivatedRoute,
+    private router:Router) { }
 
   ngOnInit() {
-  }
+    //const id=this.route.snapshot.params['id'];
+    this.route.params.subscribe((params:Params)=>{
+    this.id=+params['id'];
+  this.recipe=this.recipeService.getRecipe(this.id);})
+    // + is used to convert string id from routes url to Numbersnapshot was not used 
+    // as that only works when page Reloads and doesnt monitor when ChangeDetectionStrategywithout refreshing the page
 
+  }
+  onEditRecipe(){
+this.router.navigate(['edit'],{relativeTo:this.route})
+  }
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
